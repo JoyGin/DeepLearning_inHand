af849887757b94206312cb8574c71ebe124e4bf0
mazeqi <2631194340@qq.com>
2020-01-16 20:29:06 +0800

    first_6

--- Linear_regress/3_3.py
+++ Linear_regress/3_3.py
@@ -20,7 +20,11 @@ labels += torch.tensor(np.random.normal(0, 0.01, size=labels.size()), dtype=torc
 batch_size = 10
 dataset = Data.TensorDataset(features, labels) # 将训练数据的特征和标签组合
 data_iter = Data.DataLoader(dataset, batch_size, shuffle=True) # 随机读取小批量
+# print(dataset.tensors)
 '''
+for i in dataset:
+    print(i)
+
 for X, y in data_iter:
     print(X, y)
     break
@@ -59,21 +63,27 @@ net = nn.Sequential(OrderedDict([
 print(net)
 print(net[0])
 '''
+# 定义模型
+class LinearNet(nn.Module):
+    def __init__(self, n_feature):
+        super(LinearNet, self).__init__()
+        self.linear = nn.Linear(n_feature, 1)
 
-net = nn.Sequential(OrderedDict([
-          ('linear', nn.Linear(num_inputs, 1))
-          # ......
-        ]))
+    def forward(self, x):
+        y = self.linear(x)
+        return y
 
-# print(net)
+
+net = LinearNet(num_inputs)
+print(net)
 # print(net[0])
 
 
 
 # 初始化模型参数
-init.normal_(net[0].weight, mean=0, std=0.01)
-init.constant_(net[0].bias, val=0)  # 也可以直接修改bias的data: net[0].bias.data.fill_(0)
-
+# print('weight:',net[0].weight)
+init.normal_(net.linear.weight, mean=0, std=0.01)
+init.constant_(net.linear.bias, val=0)  # 也可以直接修改bias的data: net[0].bias.data.fill_(0)
 
 
 # 定义损失函数
@@ -81,24 +91,28 @@ loss = nn.MSELoss()
 
 # 定义优化算法
 optimizer = optim.SGD(net.parameters(), lr=0.03)
-# print(optimizer)
-
+'''
+for i in net.parameters():
+    print(i)
+'''
 # 调整学习率
 for param_group in optimizer.param_groups:
     param_group['lr'] *= 0.1 # 学习率为之前的0.1倍
 
 # 训练模型
-num_epochs = 6
+num_epochs = 100
 for epoch in range(1, num_epochs + 1):
     for X, y in data_iter:
         output = net(X)
         l = loss(output, y.view(-1, 1))
+        # print(l)
         optimizer.zero_grad() # 梯度清零，等价于net.zero_grad()
+        # print(optimizer)
         l.backward()
         optimizer.step()
     print('epoch %d, loss: %f' % (epoch, l.item()))
 
 
-dense = net[0]
+dense = net.linear
 print(true_w,'\n', dense.weight)
 print(true_b, '\n',dense.bias)
