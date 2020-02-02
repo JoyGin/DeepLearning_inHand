3fa6396c1a47ef9ea0a861767faf89881a773ecb
Mazeqi <2631194340@qq.com>
2020-02-02 15:18:03 +0800

    2020/2/2/2

---
+++ model_select/3_11.py
@@ -0,0 +1,64 @@
+import torch
+import numpy as np
+import sys
+sys.path.append("..")
+import d2lzh_pytorch as d2l
+
+# 生成人工数据集
+# y=1.2x−3.4x^2+5.6x^3 +5+ϵ,
+n_train, n_test, true_w, true_b = 100, 100, [1.2, -3.4, 5.6], 5
+features = torch.randn((n_train + n_test, 1))
+poly_features = torch.cat((features, torch.pow(features, 2), torch.pow(features, 3)), 1)
+labels = (true_w[0] * poly_features[:, 0] + true_w[1] * poly_features[:, 1]
+          + true_w[2] * poly_features[:, 2] + true_b)
+labels += torch.tensor(np.random.normal(0, 0.01, size=labels.size()), dtype=torch.float)
+
+# 定义、训练、测试模型
+# 本函数已保存在d2lzh_pytorch包中方便以后使用
+def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
+             legend=None, figsize=(3.5, 2.5)):
+    d2l.set_figsize(figsize)
+    d2l.plt.xlabel(x_label)
+    d2l.plt.ylabel(y_label)
+    d2l.plt.semilogy(x_vals, y_vals)
+    if x2_vals and y2_vals:
+        d2l.plt.semilogy(x2_vals, y2_vals, linestyle=':')
+        d2l.plt.legend(legend)
+    d2l.plt.show()
+
+num_epochs, loss = 100, torch.nn.MSELoss()
+
+def fit_and_plot(train_features, test_features, train_labels, test_labels):
+    net = torch.nn.Linear(train_features.shape[-1], 1)
+    # 通过Linear文档可知，pytorch已经将参数初始化了，所以我们这里就不手动初始化了
+
+    batch_size = min(10, train_labels.shape[0])
+    dataset = torch.utils.data.TensorDataset(train_features, train_labels)
+    train_iter = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True)
+
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+    train_ls, test_ls = [], []
+    for _ in range(num_epochs):
+        for X, y in train_iter:
+            l = loss(net(X), y.view(-1, 1))
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+        train_labels = train_labels.view(-1, 1)
+        test_labels = test_labels.view(-1, 1)
+        train_ls.append(loss(net(train_features), train_labels).item())
+        test_ls.append(loss(net(test_features), test_labels).item())
+    print('final epoch: train loss', train_ls[-1], 'test loss', test_ls[-1])
+    semilogy(range(1, num_epochs + 1), train_ls, 'epochs', 'loss',
+             range(1, num_epochs + 1), test_ls, ['train', 'test'])
+    print('weight:', net.weight.data,
+          '\nbias:', net.bias.data)
+
+# 三阶函数拟合
+# fit_and_plot(poly_features[:n_train, :], poly_features[n_train:, :],labels[:n_train], labels[n_train:])
+
+# 线性函数拟合
+# fit_and_plot(features[:n_train, :], features[n_train:, :], labels[:n_train],labels[n_train:])
+
+# 训练样本不足
+fit_and_plot(poly_features[0:2, :], poly_features[n_train:, :],labels[0:2], labels[n_train:])
