3e0285e559c88e46d27a0fa5600d036e05e0d820
Mazeqi <2631194340@qq.com>
2020-02-04 14:04:08 +0800

    2020/2/4/01

--- Kaggle/House_Prices.py
+++ Kaggle/House_Prices.py
@@ -16,4 +16,143 @@ test_data = pd.read_csv('house_prices/test.csv')
 我们将所有的训练数据和测试数据的79个特征按样本连结。
 '''
 
-all_features = pd.concat(train_data.iloc[:, 1:-1], test_data.iloc[:, 1:])
+all_features = pd.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
+
+# 预处理数据,归一化
+# 设该特征在整个数据集上的均值为μμ，标准差为σ。
+# 那么，我们可以将该特征的每个值先减去μ，
+# μ再除以σσ得到标准化后的每个特征值。
+numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+all_features[numeric_features] = all_features[numeric_features].apply(
+    lambda x: (x - x.mean()) / (x.std()))
+
+# 标准化后，每个数值特征的均值变为0，所以可以直接用0来替换缺失值
+all_features[numeric_features] = all_features[numeric_features].fillna(0)
+
+
+# dummy_na=True将缺失值也当作合法的特征值并为其创建指示特征
+all_features = pd.get_dummies(all_features, dummy_na=True)
+# print(all_features.shape) # (2919, 331)
+
+
+# 通过values属性得到NumPy格式的数据，并转成Tensor方便后面的训练。
+n_train = train_data.shape[0]
+train_features = torch.tensor(all_features[:n_train].values, dtype=torch.float)
+test_features = torch.tensor(all_features[n_train:].values, dtype=torch.float)
+train_labels = torch.tensor(train_data.SalePrice.values, dtype=torch.float).view(-1, 1)
+
+
+# 训练模型
+# 使用一个基本的线性回归模型和平方损失函数来训练模型。
+# 损失函数
+loss = torch.nn.MSELoss()
+# loss = torch.nn.CrossEntropyLoss()
+
+
+def get_net(feature_num):
+    num_inputs, num_outputs, num_hiddens1, num_hiddens2 = feature_num, 1, 256, 256
+    net = nn.Linear(feature_num, 1)
+    '''
+    net = nn.Sequential(
+        d2l.FlattenLayer(),
+        nn.Linear(num_inputs, num_hiddens1),
+        nn.ReLU(),
+        nn.Linear(num_hiddens2, num_outputs)
+    )'''
+    for param in net.parameters():
+        nn.init.normal_(param, mean=0, std=0.01)
+    return net
+
+
+# 定义比赛用来评价模型的对数均方根误差
+def log_rmse(net, features, labels):
+    with torch.no_grad():
+        # 将小于1的值设成1，使得取对数时数值更稳定
+        clipped_preds = torch.max(net(features), torch.tensor(1.0))
+        rmse = torch.sqrt(2 * loss(clipped_preds.log(), labels.log()).mean())
+    return rmse.item()
+
+
+def train(net, train_features, train_labels, test_features, test_labels,
+          num_epochs, learning_rate, weight_decay, batch_size):
+    train_ls, test_ls = [], []
+    dataset = torch.utils.data.TensorDataset(train_features, train_labels)
+    train_iter = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True)
+    # 这里使用了Adam优化算法
+    optimizer = torch.optim.Adam(params=net.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    net = net.float()
+    for epoch in range(num_epochs):
+        for X, y in train_iter:
+            l = loss(net(X.float()), y.float())
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+        train_ls.append(log_rmse(net, train_features, train_labels))
+        if test_labels is not None:
+            test_ls.append(log_rmse(net, test_features, test_labels))
+    return train_ls, test_ls
+
+
+# k折交叉验证
+def get_k_fold_data(k, i, X, y):
+    # 返回第i折交叉验证时所需要的训练和验证数据
+    assert k > 1
+    fold_size = X.shape[0] // k
+    X_train, y_train = None, None
+    for j in range(k):
+        idx = slice(j * fold_size, (j + 1) * fold_size)
+        X_part, y_part = X[idx, :], y[idx]
+        if j == i:
+            X_valid, y_valid = X_part, y_part
+        elif X_train is None:
+            X_train, y_train = X_part, y_part
+        else:
+            X_train = torch.cat((X_train, X_part), dim=0)
+            y_train = torch.cat((y_train, y_part), dim=0)
+    return X_train, y_train, X_valid, y_valid
+
+
+# 在K折交叉验证中我们训练K次并返回训练和验证的平均误差。
+def k_fold(k, X_train, y_train, num_epochs,
+           learning_rate, weight_decay, batch_size):
+    train_l_sum, valid_l_sum = 0, 0
+    for i in range(k):
+        data = get_k_fold_data(k, i, X_train, y_train)
+        net = get_net(X_train.shape[1])
+        train_ls, valid_ls = train(net, *data, num_epochs, learning_rate,
+                                   weight_decay, batch_size)
+        train_l_sum += train_ls[-1]
+        valid_l_sum += valid_ls[-1]
+        if i == 0:
+            d2l.semilogy(range(1, num_epochs + 1), train_ls, 'epochs', 'rmse',
+                         range(1, num_epochs + 1), valid_ls,
+                         ['train', 'valid'])
+        print('fold %d, train rmse %f, valid rmse %f' % (i, train_ls[-1], valid_ls[-1]))
+    return train_l_sum / k, valid_l_sum / k
+
+
+# 模型选择
+k, num_epochs, lr, weight_decay, batch_size = 10, 100, 5, 3, 64
+train_l, valid_l = k_fold(k, train_features, train_labels, num_epochs, lr, weight_decay, batch_size)
+print('%d-fold validation: avg train rmse %f, avg valid rmse %f' % (k, train_l, valid_l))
+
+
+# 用于再kaggle提交数据
+def train_and_pred(train_features, test_features, train_labels, test_data,
+                   num_epochs, lr, weight_decay, batch_size):
+    net = get_net(train_features.shape[1])
+    train_ls, _ = train(net, train_features, train_labels, None, None,
+                        num_epochs, lr, weight_decay, batch_size)
+    d2l.semilogy(range(1, num_epochs + 1), train_ls, 'epochs', 'rmse')
+    print('train rmse %f' % train_ls[-1])
+    preds = net(test_features).detach().numpy()
+    test_data['SalePrice'] = pd.Series(preds.reshape(1, -1)[0])
+    submission = pd.concat([test_data['Id'], test_data['SalePrice']], axis=1)
+    submission.to_csv('house_prices/submission.csv', index=False)
+
+
+train_and_pred(train_features, test_features, train_labels, test_data, num_epochs, lr, weight_decay, batch_size)
+
+
+
+
