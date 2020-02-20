f31ab324dee064d615fe8465f87ed76bf9df265a
Mazeqi <2631194340@qq.com>
2020-02-20 23:04:32 +0800

    2020/2/20/02

---
+++ Recurrent_Neural_Network/6_8LSTM_By_Pytorch.py
@@ -0,0 +1,26 @@
+import numpy as np
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+
+import sys
+import d2lzh_pytorch as d2l
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+(corpus_indices, char_to_idx, idx_to_char, vocab_size) = d2l.load_data_jay_lyrics()
+
+num_inputs, num_hiddens, num_outputs = vocab_size, 256, vocab_size
+print('will use', device)
+
+
+num_epochs, num_steps, batch_size, lr, clipping_theta = 160, 35, 32, 1e2, 1e-2
+pred_period, pred_len, prefixes = 40, 50, ['分开', '不分开']
+
+
+lr = 1e-2  # 注意调整学习率
+gru_layer = nn.LSTM(input_size=vocab_size, hidden_size=num_hiddens)
+model = d2l.RNNModel(gru_layer, vocab_size).to(device)
+d2l.train_and_predict_rnn_pytorch(model, num_hiddens, vocab_size, device,
+                                corpus_indices, idx_to_char, char_to_idx,
+                                num_epochs, num_steps, lr, clipping_theta,
+                                batch_size, pred_period, pred_len, prefixes)
