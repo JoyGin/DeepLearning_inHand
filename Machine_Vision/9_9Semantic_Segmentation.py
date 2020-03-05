2ef8ba13a4d7c69d4787df9f3b2adcf1f6773afc
Mazeqi <2631194340@qq.com>
2020-03-05 23:02:02 +0800

    2020/3/5/02

--- Machine_Vision/9_9Semantic_Segmentation.py
+++ Machine_Vision/9_9Semantic_Segmentation.py
@@ -2,6 +2,7 @@ import time
 import torch
 import torch.nn.functional as F
 import torchvision
+from torchvision import transforms
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
@@ -17,10 +18,121 @@ def read_voc_images(root="../Data/VOCdevkit/VOC2012",
     if max_num is not None:
         images = images[:min(max_num, len(images))]
     features, labels = [None] * len(images), [None] * len(images)
-    for i, fname in tqdm(enumerate(images)):
+    for i, fname in enumerate(images):
         features[i] = Image.open('%s/JPEGImages/%s.jpg' % (root, fname)).convert("RGB")
         labels[i] = Image.open('%s/SegmentationClass/%s.png' % (root, fname)).convert("RGB")
-    return features, labels # PIL image
+    return features, labels  # PIL image
 
 voc_dir = "../Data/VOCdevkit/VOC2012"
 train_features, train_labels = read_voc_images(voc_dir, max_num=100)
+
+n = 5
+imgs = train_features[0:n] + train_labels[0:n]
+# d2l.show_images(imgs, 2, n)
+# d2l.plt.show()
+
+VOC_COLORMAP = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
+                [0, 0, 128], [128, 0, 128], [0, 128, 128], [128, 128, 128],
+                [64, 0, 0], [192, 0, 0], [64, 128, 0], [192, 128, 0],
+                [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
+                [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0],
+                [0, 64, 128]]
+
+VOC_CLASSES = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
+               'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
+               'diningtable', 'dog', 'horse', 'motorbike', 'person',
+               'potted plant', 'sheep', 'sofa', 'train', 'tv/monitor']
+
+
+colormap2label = torch.zeros(256 ** 3, dtype=torch.uint8)
+for i, colormap in enumerate(VOC_COLORMAP):
+    colormap2label[(colormap[0] * 256 + colormap[1]) * 256 + colormap[2]] = i
+
+
+def voc_label_indices(colormap, colormap2label):
+    """
+    convert colormap (PIL image) to colormap2label (uint8 tensor).
+    """
+    colormap = np.array(colormap.convert("RGB")).astype('int32')
+    idx = ((colormap[:, :, 0] * 256 + colormap[:, :, 1]) * 256
+           + colormap[:, :, 2])
+    return colormap2label[idx]
+
+
+y = voc_label_indices(train_labels[0], colormap2label)
+# print(y[105:115, 130:140], VOC_CLASSES[1])
+
+
+# 预处理数据
+def voc_rand_crop(feature, label, height, width):
+    """
+    Random crop feature (PIL image) and label (PIL image).
+    """
+    i, j, h, w = torchvision.transforms.RandomCrop.get_params(
+            feature, output_size=(height, width))
+
+    feature = torchvision.transforms.functional.crop(feature, i, j, h, w)
+    label = torchvision.transforms.functional.crop(label, i, j, h, w)
+
+    return feature, label
+
+imgs = []
+for _ in range(n):
+    imgs += voc_rand_crop(train_features[0], train_labels[0], 200, 300)
+# d2l.show_images(imgs[::2] + imgs[1::2], 2, n);
+# d2l.plt.show()
+
+
+class VOCSegDataset(torch.utils.data.Dataset):
+    def __init__(self, is_train, crop_size, voc_dir, colormap2label, max_num=None):
+        """
+        crop_size: (h, w)
+        """
+        self.rgb_mean = np.array([0.485, 0.456, 0.406])
+        self.rgb_std = np.array([0.229, 0.224, 0.225])
+        self.tsf = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize(mean=self.rgb_mean,
+                                             std=self.rgb_std)
+        ])
+
+        self.crop_size = crop_size # (h, w)
+        features, labels = read_voc_images(root=voc_dir,
+                                           is_train=is_train,
+                                           max_num=max_num)
+        self.features = self.filter(features) # PIL image
+        self.labels = self.filter(labels)     # PIL image
+        self.colormap2label = colormap2label
+        print('read ' + str(len(self.features)) + ' valid examples')
+
+    def filter(self, imgs):
+        return [img for img in imgs if (
+            img.size[1] >= self.crop_size[0] and
+            img.size[0] >= self.crop_size[1])]
+
+    def __getitem__(self, idx):
+        feature, label = voc_rand_crop(self.features[idx], self.labels[idx],
+                                       *self.crop_size)
+
+        return (self.tsf(feature), # float32 tensor
+                voc_label_indices(label, self.colormap2label)) # uint8 tensor
+
+    def __len__(self):
+        return len(self.features)
+
+
+crop_size = (320, 480)
+max_num = 100
+voc_train = VOCSegDataset(True, crop_size, voc_dir, colormap2label, max_num)
+voc_test = VOCSegDataset(False, crop_size, voc_dir, colormap2label, max_num)
+
+batch_size = 64
+num_workers = 0
+train_iter = torch.utils.data.DataLoader(voc_train, batch_size, shuffle=True,
+                              drop_last=True, num_workers=num_workers)
+test_iter = torch.utils.data.DataLoader(voc_test, batch_size, drop_last=True,
+                             num_workers=num_workers)
+for X, Y in train_iter:
+    print(X.dtype, X.shape)
+    print(y.dtype, Y.shape)
+    break
--- d2lzh_pytorch/utils.py
+++ d2lzh_pytorch/utils.py
@@ -688,11 +688,17 @@ class Benchmark():
 def show_images(imgs, num_rows, num_cols, scale=2):
     figsize = (num_cols * scale, num_rows * scale)
     _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
-    for i in range(num_rows):
+    if(num_rows == 1):
         for j in range(num_cols):
-            axes[i][j].imshow(imgs[i * num_cols + j])
-            axes[i][j].axes.get_xaxis().set_visible(False)
-            axes[i][j].axes.get_yaxis().set_visible(False)
+            axes[j].imshow(imgs[j])
+            axes[j].axes.get_xaxis().set_visible(False)
+            axes[j].axes.get_yaxis().set_visible(False)
+    else:
+        for i in range(num_rows):
+            for j in range(num_cols):
+                axes[i][j].imshow(imgs[i * num_cols + j])
+                axes[i][j].axes.get_xaxis().set_visible(False)
+                axes[i][j].axes.get_yaxis().set_visible(False)
     return axes
 
 def train(train_iter, test_iter, net, loss, optimizer, device, num_epochs):
